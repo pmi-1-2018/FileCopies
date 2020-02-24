da68542b37fb5ed0741574fa41996fb5acbed96d
Andriy Oleksyuk <andre200129@gmail.com>
2020-02-24 22:29:21 +0200

    Created base classes

---
+++ FileCopy/FIle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCopy
+{
+    class File
+    {
+        public File()
+        {
+            name = "unknown";
+            directory = "unknown";
+        }
+        public File(string n, string dir)
+        {
+            name = n;
+            directory = dir;
+        }
+
+        public string name { get; }
+        public string directory { get; }
+        public override string ToString()
+        {
+            return name + '\\' + directory;
+        }
+    }
+}
---
+++ FileCopy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
